4963ca268b02b033f4a912e3b90a7e606dbc3964
adibyju <20d070004@iitb.ac.in>
2021-10-29 16:04:23 +0530

    Added YourCart component

--- orezano_react_folder/src/Components/YourCart.js
+++ orezano_react_folder/src/Components/YourCart.js
@@ -8,17 +8,52 @@ const YourCart = () => {
                 <h2 className={YCCSS.heading}>Your Cart</h2>
                 <hr className={YCCSS.sep}/>
             </div>
+
             <div className={YCCSS.modOfOrd}>Mode of order : Delivery</div>
+
             <YourCartCard/>
             <YourCartCard/>
-            <YourCartCard/>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            {/* <YourCartCard/> */}
+
+            <div className={YCCSS.detailsDiv}>
+                <h3>Your Details</h3> 
+                <div className={YCCSS.detailRowDiv}><input type="text" placeholder="Adi" className={YCCSS.detailRow} /></div>
+                <div className={YCCSS.detailRowDiv}><input pattern="[0-9]{10}" placeholder="1234567890" className={YCCSS.detailRow}/></div>
+                <div className={YCCSS.detailRowDiv}><input type="text" placeholder="H9, IIT Bombay" className={YCCSS.detailRow}/></div>              
+
             </div>
-            <div className={YCCSS.orderDiv}><button className={YCCSS.placeOrder}><div className={YCCSS.container}><div className={YCCSS.one}><i className="fa fa-inr" aria-hidden="true"></i>222.00</div><div className={YCCSS.two}>TOTAL</div><div className={YCCSS.three}><span className={YCCSS.placeOrd}>Place Order<i className={`fas fa-angle-right ${YCCSS.arrow}`}></i></span></div></div></button></div>
+
+            <div className={YCCSS.chargesContainer}>
+                <div className={YCCSS.charges}>
+                    <div className={YCCSS.chargesDiv}>
+                        <div className={YCCSS.floatLeft}>Item Total</div>
+                        <div className={YCCSS.floatRight}><i className="fa fa-inr" aria-hidden="true"/>159.00</div>
+                    </div>
+                    <div className={YCCSS.chargesDiv}>
+                        <div className={YCCSS.floatLeft}>Packaging charges</div>
+                        <div className={YCCSS.floatRight}><i className="fa fa-inr" aria-hidden="true"/>62.95</div>
+                    </div>
+                    <div className={YCCSS.chargesDiv}>
+                        <div className={YCCSS.floatLeft}>Taxes and charges</div>
+                        <div className={YCCSS.floatRight}><i className="fa fa-inr" aria-hidden="true"/>0.00</div>
+                    </div>
+                    <div className={`${YCCSS.chargesDivBold} ${YCCSS.chargesDiv}`}>
+                        <div className={YCCSS.floatLeft}>Grand Total</div>
+                        <div className={YCCSS.floatRight}><i className="fa fa-inr" aria-hidden="true"/>222.00</div>
+                    </div>               
+                    
+                </div>
+            </div>
+            
+            <div className={YCCSS.orderDiv}>
+                <button className={YCCSS.placeOrder}>
+                    <div className={YCCSS.container}>
+                        <div className={YCCSS.one}><i className="fa fa-inr" aria-hidden="true"></i>222.00</div>
+                        <div className={YCCSS.two}>TOTAL</div>
+                        <div className={YCCSS.three}><span className={YCCSS.placeOrd}>Place Order<i className={`fas fa-angle-right ${YCCSS.arrow}`}></i></span></div>
+                    </div>
+                </button>
+            </div>
         </div>
     )
 }
--- orezano_react_folder/src/Components/YourCartCard.js
+++ orezano_react_folder/src/Components/YourCartCard.js
@@ -7,7 +7,7 @@ const YourCartCard = () => {
             <div className={YCCCSS.container}>
                 <div className={YCCCSS.dish}><img src={nonVeg} alt="" className={YCCCSS.type} />
                    <p className={YCCCSS.dishDesc}>Chicken Pepperoni Stuffed Garlic Bread</p></div>
-                   <div className={YCCCSS.price}><i className="fa fa-inr" aria-hidden="true"></i>159</div>
+                   <div className={YCCCSS.price}><i className="fa fa-inr" aria-hidden="true"></i>159.00</div>
             </div>
         </div>
         
