f8a41e16e5160f8edc59e2d5773c3d8d600ca791
adibyju <20d070004@iitb.ac.in>
2021-11-29 12:06:49 +0530

    Update App.js

--- orezano_react_folder/src/App.js
+++ orezano_react_folder/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import SignInUp from './Components/SignInUp'
-import OrderSum from './Components/OrderSum'
-import YourCart from './Components/YourCart'
-import LandingPage from './Components/LandingPage'
-import Profile from './Components/Profile'
+import SignInUp from './Components/SignInUp/SignInUp'
+import OrderSum from './Components/OrderSum/OrderSum'
+import YourCart from './Components/YourCart/YourCart'
+import LandingPage from './Components/LandingPage/LandingPage'
+import Profile from './Components/Profile/Profile.js'
 
 function App() {
   return (
