371c5815f7785dd52bc93465815fd9ddb454824a
adibyju <20d070004@iitb.ac.in>
2021-11-29 12:08:46 +0530

    Change organization of files

---
+++ orezano_react_folder/src/Components/OrderSum/OrderSum.js
@@ -0,0 +1,19 @@
+import {useRef} from 'react'
+import OSCSS from './OrderSum.module.css'
+import Card from '../Card/Card.js'
+
+const OrderSum = () => {
+    return (
+        <div className={OSCSS.container}>
+            <div className={`justify-content-center ${OSCSS.title}`}>
+                <h2 className={OSCSS.heading}>Order Summary</h2>
+            </div>  
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    )
+}
+
+export default OrderSum
