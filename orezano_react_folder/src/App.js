6852be94c4fab9662222702933cedd38a91e7530
adibyju <20d070004@iitb.ac.in>
2021-11-30 13:17:10 +0530

    Update changes

--- orezano_react_folder/src/App.js
+++ orezano_react_folder/src/App.js
@@ -5,14 +5,16 @@ import YourCart from './Components/YourCart/YourCart'
 import LandingPage from './Components/LandingPage/LandingPage'
 import Profile from './Components/Profile/Profile'
 import Header from './Components/Header/Header'
+import ResetPass from './Components/ResetPass/ResetPass'
 
 function App() {
   return (
     <div className="App">
-      <SignInUp /> 
+      {/* <SignInUp />  */}
       {/* <OrderSum /> */}
-      {/* <Header/>
-      <Profile /> */}
+      {/* <Header/> */}
+      {/* <Profile /> */}
+      <ResetPass />
       {/* <YourCart />  */}
     </div>
   );
--- orezano_react_folder/src/Components/Header/Header.js
+++ orezano_react_folder/src/Components/Header/Header.js
@@ -1,18 +1,32 @@
 import HCSS from './Header.module.css'
 import accImg from '../Images/account.png';
+import $ from 'jquery';
 
 const Header = () => {
+    // $(function() {
+    //     var txt = screen.width < 400 ? 'Search...' : 'Search your favourite dishes...';
+    //     $('#sBox').attr('placeholder', txt);
+    // });
+
     return (
         <div>
             <div className={HCSS.headerDiv}>
+                <div className={HCSS.logoContainer}>
                 <a className={HCSS.logoLink} href="#">
                     <img className={HCSS.logo} src={accImg} alt="" />
                 </a>
-                <div className={HCSS['input-field']}>
-                <i className="fas fa-search"></i>
-                <input type="text" placeholder="Search..." />
                 </div>
-                <img className={HCSS.menu} src={accImg} alt="" />
+
+                <div className={HCSS.searchContainer}>
+                <div className={HCSS.inputField}>
+                    <i className={`fas fa-search ${HCSS.searchIcon}`}></i>
+                <input id="sBox" className={HCSS.searchBox} type="text" placeholder="Search your favourite dishes..." />
+                </div>
+                </div>                
+
+                <div className={HCSS.menuContainer}>
+                    <img className={HCSS.menu} src={accImg} alt="" />
+                </div>                
             </div>
         </div>
     )
